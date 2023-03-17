8a1202c8b230b55225f1cf9aaf9f6c8caaaa78ac
congminhle44 <nhokdevil123@gmail.com>
2023-03-17 11:52:27 +0700

    first commit

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -11,9 +11,9 @@ const pointLightPositions = [
 ];
 
 const spotLightProperties = [
-  { position: [-3.35, 2.4, 4.2] },
-  { position: [-3.35, 2.2, 4.82] },
-  { position: [-3.35, 2.2, 6.138] },
+  // { position: [-3.35, 2.4, 4.2] },
+  { position: [-3.35, 2.4, 5.35] },
+  // { position: [-3.35, 2.2, 6.58] },
 ];
 
 const Ground = ({ ...props }) => {
@@ -27,7 +27,17 @@ const Ground = ({ ...props }) => {
   };
   const renderSpotLight = () => {
     return spotLightProperties.map((property) => {
-      return <spotLight {...property} color="FFF8DA" intensity={1} />;
+      return (
+        <spotLight
+          {...property}
+          angle={45}
+          penumbra={0.5}
+          target={{ position: [-1, 0, 0] }}
+          decay={1.0}
+          color="FFF8DA"
+          intensity={1}
+        />
+      );
     });
   };
 
