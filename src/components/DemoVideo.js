52a8df1cebf3bba570c58fe1ef681c29003de845
congminhle44 <nhokdevil123@gmail.com>
2023-04-11 16:12:18 +0700

    refactor: video texture

--- src/components/DemoVideo.js
+++ src/components/DemoVideo.js
@@ -6,29 +6,19 @@ const DemoVideo = memo(function MemoVideo() {
   const video = document.createElement("video");
   video.src = "/assets/GuildMovie_01.mp4";
   video.loop = true;
+  video.autoplay = true;
   video.muted = "muted";
-  video.load();
   video.play();
 
   //make your video canvas
   const videocanvas = document.createElement("canvas");
   const videocanvasctx = videocanvas.getContext("2d");
-
-  //set its size
-  videocanvas.width = 1280;
-  videocanvas.height = 720;
-
-  //draw a black rectangle so that your spheres don't start out transparent
-  videocanvasctx.fillStyle = "#000000";
-  videocanvasctx.fillRect(0, 0, videocanvas.width, videocanvas.height);
-  const videoTexture = new THREE.Texture(videocanvas);
+  const videoTexture = new THREE.VideoTexture(video);
 
   useFrame(() => {
     if (video.readyState === video.HAVE_ENOUGH_DATA) {
       //draw video to canvas starting from upper left corner
       videocanvasctx.drawImage(video, 0, 0);
-      //tell texture object it needs to be updated
-      videoTexture.needsUpdate = true;
     }
   });
 
